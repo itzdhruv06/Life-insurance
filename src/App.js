27114c52e36f93ec0d674f388f534e1cad2d1fbb
Dhanesh Vishe <dhaneshvishe06@gmail.com>
2025-07-23 12:26:29 +0530

    initial github setup

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div>
       <h1 className='mt-24 font-bold'>hello Tailwind</h1>
       <p className='text-red-900 mx-48'>browser</p>
+      <h1 className="text-5xl">Kolhapur</h1>
     </div>
   );
 }
